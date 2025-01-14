270991dfd09980d3d4f20ac796c1a3d93c89f919
Falko.com <141217738+falko-00@users.noreply.github.com>
2025-01-14 19:48:26 +0100

    Update server.lua

--- falcon/server.lua
+++ falcon/server.lua
@@ -1,8 +1,15 @@
 -- server.lua
-RegisterCommand('playVideo', function(source, args, rawCommand)
-    -- URL des Videos
-    local videoUrl = "https://shorturl.at/f7dFD"  -- Hier die Video-URL einfügen
 
-    -- Event an alle Clients senden
-    TriggerClientEvent('playVideoForAll', -1, videoUrl)  -- -1 bedeutet alle Spieler
+-- Befehl zum Abspielen des Videos an alle Spieler
+RegisterCommand('playVideo', function(source, args, rawCommand)
+    -- YouTube-URL zum Video
+    local youtubeUrl = 'https://www.youtube.com/watch?v=_yZXPggxS18'  -- YouTube-Video-URL
+    -- Event an alle Clients senden, um das Video zu starten
+    TriggerClientEvent('playVideoForAll', -1, youtubeUrl)  -- -1 bedeutet alle Spieler
+end, false)
+
+-- Befehl zum Stoppen des Videos an alle Spieler
+RegisterCommand('stopVideo', function(source, args, rawCommand)
+    -- Event an alle Clients senden, um das Video zu stoppen
+    TriggerClientEvent('stopVideoForAll', -1)  -- -1 bedeutet alle Spieler
 end, false)
